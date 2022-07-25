dff50912b3311bc849b612764501e068da63a406
Tk <thekingofshadows98@gmail.com>
2022-07-25 13:57:40 -0600

    ball behavior

--- Proyect/Components/Ball/Ball.gd
+++ Proyect/Components/Ball/Ball.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 class_name Ball
 
+##RESET VARS
+var _start_position:Vector2 = Vector2(0,0)
+var _is_reset:bool = false
 
 ##BALL NODES VARS
 var _player_owner:MainCharacter = null
@@ -21,6 +24,7 @@ var _body_touch:Node2D = null
 
 ## PREFCONFIGS
 func _ready():
+	_start_position = transform.origin
 	linear_damp = _DAMP
 	pass
 
@@ -29,33 +33,17 @@ func set_target(target:Node2D):
 	_target = target
 
 func _physics_process(delta):
-	_check_distnace_to_body()
 	if (_target == null): return
 	var direction:Vector2 = _target.position.direction_to(position)
 	linear_velocity = direction * (_BOUNCE_FORCE * 0.5)
 	pass
 
 
-
-## TOUCH BODY FUNCTS
-func _check_distnace_to_body():
-	if(_body_touch == null): return
-	var dist = position.distance_to(_body_touch.position)
-	if(dist > _MAX_DISTANCE_TO_BODY):
-		_body_touch = null
-		print("(41) Ball: RELEASE")
-
-
 ## TOUCH RIGGIDBODY
 func _on_Ball_body_entered(body):
+	print("TOUCH")
 	if body is MainCharacter:
-		var node:Node2D = body
-		print("(48) BAll: TOUCHED")
-		_strike(node)
-	else:
-		_disable_interaction(5);
-		var node:Node2D = body
-		print("(48) BAll: Wall Touched")
+		var node:MainCharacter = body
 		_strike(node)
 	pass # Replace with function body.
 
@@ -80,12 +68,12 @@ func _on_Detector_body_exited(body):
 	pass # Replace with function body.
 
 
-
 ##PLAYER INTERACTIONS
 func strike(valid:Node2D, target:Node2D):
 	if(valid != _body_touch): return
 	print("(58) Ball: STRIKE VALID!")
 	_strike(target)
+
 
 ## PHYSICS
 func _strike(target:Node2D):
@@ -93,25 +81,24 @@ func _strike(target:Node2D):
 	linear_velocity = direction * _BOUNCE_FORCE
 
 
+## DISABLE INTERACTIONS WITH PLAYERS
+func _disable_inteactions(time):
+	pass
+
 ## INTERACTUABLE FUNCTS
 func _check_interactuable():
 	if(_is_interactuable): return
 	_is_interactuable = OS.get_unix_time() > _time_to_interactuable
-	_update_collision_shape()
 	pass
 
-## COLISION DETECTOR
-func _update_collision_shape():
-	if($"Detector/Detector Shape".disabled !=  _is_interactuable): return
-	$"Detector/Detector Shape".disabled = not _is_interactuable
-	print("(78) Ball interaction = ", $"Detector/Detector Shape".disabled)
-	pass
-
-## STOP DETECTIONS
-func _disable_interaction(time_left:float = _default_time_not_interactuable):
-	_is_interactuable = false
-	_update_collision_shape()
-	_time_to_interactuable = OS.get_unix_time() + time_left
+## RESET POSITION
+func reset_pos():
+	_is_reset = true
 	pass
 
 
+func _integrate_forces(state):
+	if _is_reset:
+		state.transform = Transform2D(0, _start_position)
+		state.linear_velocity = Vector2()
+		_is_reset = false
